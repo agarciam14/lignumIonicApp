d67db019a73f845fe6b892b43afc10bbab43cfd8
chosky <davidhenaoo98@gmail.com>
2020-03-12 19:44:40 -0500

    Se hace la pagina de test para poder continuar

--- Lignum/src/app/app.module.ts
+++ Lignum/src/app/app.module.ts
@@ -9,15 +9,24 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+// Desde aca se importan los providers
+import { ProviderSettingsProvider } from '../providers/provider-settings/provider-settings';
+import { GeolocalizacionServicesProvider } from '../providers/geolocalizacion-service/geolocalizacion-service';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    ProviderSettingsProvider,
+    GeolocalizacionServicesProvider
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
