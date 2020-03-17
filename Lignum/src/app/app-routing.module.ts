0951476d1d69eb0c9569317d9eb51a75e5826b66
Agarciam <agarciam@eafit.edu.co>
2020-03-17 18:25:13 -0500

    Creacaion de pagina de crud con estructura base

--- Lignum/src/app/app-routing.module.ts
+++ Lignum/src/app/app-routing.module.ts
@@ -9,6 +9,10 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'crud',
+    loadChildren: () => import('./crud/crud.module').then( m => m.CrudPageModule)
   }
 ];
 @NgModule({
