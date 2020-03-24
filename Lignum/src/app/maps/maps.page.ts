6c9edf103401fdd774d6424f57afa039111cfe6d
Sebastian Arboleda <sarboledab@eafit.edu.co>
2020-03-24 14:38:30 -0500

    Se adiciona el marcador y la geolocalización

--- Lignum/src/app/maps/maps.page.ts
+++ Lignum/src/app/maps/maps.page.ts
@@ -1,4 +1,5 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 declare var google;
 
 @Component({
@@ -7,20 +8,44 @@ declare var google;
   styleUrls: ['./maps.page.scss'],
 })
 export class MapsPage implements OnInit {
+  latitude: any;
+  longitude; any;
   @ViewChild('mapElement', {static:false}) mapElement: ElementRef;
-  constructor() {
-  }
+  constructor(private geolocation: Geolocation) {}
 
   ngOnInit(): void {
   }
 
   ngAfterViewInit(): void {
-    const map = new google.maps.Map(
-        this.mapElement.nativeElement,
-        {
-          center: {lat: 6.230833, lng: -75.590553},
-          zoom: 11
-        });
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.latitude = resp.coords.latitude;
+      this.longitude = resp.coords.longitude;
+      const map = new google.maps.Map(
+          this.mapElement.nativeElement,
+          {
+            center: {lat: 6.230833, lng: -75.590553},
+            zoom: 15
+          });
+          /*Localización del macador*/
+          const pos = {
+            lat: this.latitude,
+            lng: this.longitude
+          };
+          map.setCenter(pos);
+          const icon = {
+            url: 'assets/icon/user.png',
+            scaledSize: new google.maps.Size(40, 40),
+          };
+          const marker = new google.maps.Marker({
+            position: pos,
+            map: map,
+            tittle: '',
+            icon: icon
+          });
+
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });    
   }
 
 }
