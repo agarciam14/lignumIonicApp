71c5cc2c12cfe18de6d9598ee99feb33514e5b5e
chosky <davidhenaoo98@gmail.com>
2020-04-11 14:55:44 -0500

    Se testea el login y el registro de usuarios queda terminado, se añaden labels para tener una mejor identificación de cada campo

--- Lignum/src/app/ingreso/registro-usuario-nuevo/registro-usuario-nuevo.page.ts
+++ Lignum/src/app/ingreso/registro-usuario-nuevo/registro-usuario-nuevo.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 import { RegistroUsuarioNuevoServicesProvider } from '../../../providers/registro-usuario-nuevo-service/registro-usuario-nuevo-service';
 
 @Component({
@@ -13,24 +13,67 @@ export class RegistroUsuarioNuevoPage implements OnInit {
     nombre_usuario: "",
     documento: "",
     correo: "",
+    imagen: "",
     contrasena: "",
     confirmar_contrasena: ""
   }
 
-  constructor( public registroUsuarioNuevoServicesProvider: RegistroUsuarioNuevoServicesProvider) { }
+  constructor( public registroUsuarioNuevoServicesProvider: RegistroUsuarioNuevoServicesProvider, public alertController: AlertController) { }
 
   ngOnInit() {
   }
 
   crearUsuario() {
-    this.registroUsuarioNuevoServicesProvider.registroUsiarioNuevo(this.usuario).subscribe(
-      (data) => {
-        console.log(data);
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
+    if (this.validarCamposVacios()) {
+      this.mostrarAlerta('Campos vacios', 'Los campos usuario, documento y correo no deben de estar vacios.')
+    } else if (this.validarContrasenaIgual()) { 
+      this.mostrarAlerta('Contraseña', 'Las contraseñas no coinciden')
+    } else {
+      this.registroUsuarioNuevoServicesProvider.registroUsiarioNuevo(this.usuario).subscribe(
+        (data) => {
+          if (data["tipo"] == "error_correo_documento") {
+            this.mostrarAlerta('Correo o documento invalido', data['mensaje']);
+          } else if (data["tipo"] == "error_nombre_usuario") {
+            this.mostrarAlerta('Usuario existente', data['mensaje']);
+          } else if (data["tipo"] == "error_contrasena") {
+            this.mostrarAlerta('Contraseña insegura', data['mensaje']);
+          } else if (data["tipo"] == "error_interno") {
+            this.mostrarAlerta('Error interno', data['mensaje']);
+          } else if (data["tipo"] == "aprobado") {
+            this.mostrarAlerta('Aprobado', data['mensaje']);
+          }
+        }, 
+        (error) => {
+          this.mostrarAlerta('Error conexion', 'Ocurrio un error, revisa tu conexion a internet');
+        }
+      );
+    }
+  }
+
+  validarCamposVacios() {
+    if (this.usuario['nombre_usuario'] == "" || this.usuario['documento'] == "" || this.usuario['correo'] == "") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  validarContrasenaIgual() {
+    if (this.usuario['contrasena'] != this.usuario['confirmar_contrasena']) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  async mostrarAlerta(titulo, mensaje) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message: mensaje,
+      buttons: ['ACEPTAR']
+    });
+
+    await alert.present();
   }
 
 }
